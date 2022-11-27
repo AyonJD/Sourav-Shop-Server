ea3b35917c6bbdec6408e633a911d5dac1115062
Ayon Jodder <ayonjodder177@gmail.com>
2022-11-28 01:46:50 +0600

    Create user for google login

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -2,7 +2,7 @@ const UserModel = require('../Models/UserModel.js');
 const jwt = require('jsonwebtoken');
 
 module.exports.createUser = async (req, res) => {
-    const { email, password, userName, role } = await req.body;
+    const { email, password, userName, role, isGoogleUser } = await req.body;
 
     const newUser = new UserModel({
         userName,
@@ -12,12 +12,24 @@ module.exports.createUser = async (req, res) => {
     });
 
     try {
-        const accessToken = jwt.sign(req.body, process.env.TOKEN, { expiresIn: "1d" });
-        const result = await newUser.save();
-        const { userName, email } = result;
-        res.status(200).json({ success: true, message: "Successfully created user", token: accessToken, result: { userName, email, role } });
+        const newInitilizer = await req.body.email;
+        const userInitilizer = await req.body.userName;
+        const roleInitilizer = await req.body.role;
+        const isGoogleUserInitilizer = await req.body.isGoogleUser;
+        const user = await UserModel.findOne({ email: newInitilizer});
+        
+        if (user && isGoogleUserInitilizer) {
+            const accessToken = jwt.sign(req.body, process.env.TOKEN, { expiresIn: "1d" });
+            res.status(200).json({ success: true, message: "Successfully created user", token: accessToken, result: { userName: userInitilizer, email:newInitilizer, role: roleInitilizer } });
+        } else {
+            const accessToken = jwt.sign(req.body, process.env.TOKEN, { expiresIn: "1d" });
+            const result = await newUser.save();
+            const { userName, email } = result;
+            res.status(200).json({ success: true, message: "Successfully created user", token: accessToken, result: { userName, email, role } });
+        }
+        
     } catch (err) {
-        res.status(500).json({ success: false, message: "Internal server error", error: err.message });
+        res.status(500).json({ success: false, message: "Internal server errorrrr", error: err.message });
     }
 }
 
@@ -40,14 +52,14 @@ module.exports.loginUser = async (req, res) => {
         if (user) {
             if (user.password === password) {
                 const accessToken = jwt.sign(req.body, process.env.TOKEN, { expiresIn: "1d" });
-                res.status(200).json({ success: true, message: "Login successfully", token: accessToken, result: { user: user_exclude_pass } });
+                res.status(200).json({status:200, success: true, message: "Login successfully", token: accessToken, result: { user: user_exclude_pass } });
             } else {
-                res.status(401).json({ success: false, message: "Invalid password" });
+                res.status(401).json({status:401, success: false, message: "Invalid password" });
             }
         } else {
-            res.status(404).json({ success: false, message: "User not found" });
+            res.status(404).json({status:404, success: false, message: "User not found" });
         }
     } catch (err) {
-        res.status(500).json({ success: false, message: "Internal server error", error: err.message });
+        res.status(500).json({status:500, success: false, message: "Internal server error", error: err.message });
     }
 }
