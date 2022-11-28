dbb6355bd62e035acccd75d20a5c535057cf52d9
Ayon Jodder <ayonjodder177@gmail.com>
2022-11-28 14:49:50 +0600

    Single user controller

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { createUser, getUser, loginUser } = require('../Controllers/UserController.js');
+const { createUser, getUser, loginUser, getSingleUser } = require('../Controllers/UserController.js');
 const router = express.Router();
 
 router.get('/', getUser);
 router.post('/signup', createUser)
 router.post('/login', loginUser)
+router.get('/:id', getSingleUser)
 
 module.exports = router;
